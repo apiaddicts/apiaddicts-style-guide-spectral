92bf3aebeedaa53af9c8f9c5bd7b0c0d81815ff8
Omar del Valle Rodriguez <omar@odelvalle.com>
2024-10-29 19:20:06 +0100

    Add test to ruleset

---
+++ test/oap3/resources/OAR014/fail-limit-ressorces-paths.js
@@ -0,0 +1,72 @@
+module.exports = {
+  "openapi": "3.0.0",
+  "info": {
+    "version": "1.0.0",
+    "title": "Swagger Petstore"
+  },
+  "paths": {
+    "/one": {
+      "get": {
+        "responses": {
+          "default": {
+            "description": "Ok"
+          }
+        }
+      }
+    },
+    "/one/two": {
+      "get": {
+        "responses": {
+          "default": {
+            "description": "Ok"
+          }
+        }
+      }
+    },
+    "/one/two/three": {
+      "get": {
+        "responses": {
+          "default": {
+            "description": "Ok"
+          }
+        }
+      }
+    },
+    "/one/two/three/four": {
+      "get": {
+        "responses": {
+          "default": {
+            "description": "Ok"
+          }
+        }
+      }
+    },
+    "/one/two/three/four/five": {
+      "get": {
+        "responses": {
+          "default": {
+            "description": "Ok"
+          }
+        }
+      }
+    },
+    "/one/two/three/four/five/six": {
+      "get": {
+        "responses": {
+          "default": {
+            "description": "Ok"
+          }
+        }
+      }
+    },
+    "/one/two/three/four/five/six/seven": {
+      "get": {
+        "responses": {
+          "default": {
+            "description": "Ok."
+          }
+        }
+      }
+    }
+  }
+}
