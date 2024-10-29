03957212d5fc4eaebff7e9b329fdbf9bcf155593
Omar del Valle Rodriguez <omar@odelvalle.com>
2024-10-29 14:50:16 +0100

    014 warn on max path parts

---
+++ functions/apq-resources.js
@@ -0,0 +1,14 @@
+
+export default (given, { max }) => {
+  const result = [];
+  if (given === null) return result;
+
+  const parts = given.substr(1).split('/');
+  if (parts.length === max) {
+    result.push({
+      message: `The number of parts of the ${given} must be less than 5`,
+    });
+  }
+
+  return result;
+};
