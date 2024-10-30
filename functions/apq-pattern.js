91569a168280e72466ba7ea3138f4d5eca2345ba
Omar del Valle Rodriguez <omar@odelvalle.com>
2024-10-30 21:59:37 +0100

    remove test function

--- functions/apq-pattern.js
+++
@@ -1,7 +0,0 @@
-module.exports = (given, { match }, context) => {
-  const result = [];
-  if (given === null) return result;
-
-
-  return given.match(match) ? result : [{ message: context.rule.message }];
-};
