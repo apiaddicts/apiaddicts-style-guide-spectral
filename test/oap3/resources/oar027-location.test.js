2e0e57bce43e0c79a8bbfdcc99fdb532acdbda17
Omar del Valle <omar@odelvalle.com>
2024-12-05 12:40:59 +0100

    fix rule 027

---
+++ test/oap3/resources/oar027-location.test.js
@@ -0,0 +1,24 @@
+const { linterForRule } = require('../../helpers/utils');
+
+let linter;
+
+const oar027fail = require('./OAR027/fail-post-201-location');
+const oar027ok = require('./OAR027/ok-post-201-location');
+
+beforeAll(async () => {
+  linter = await linterForRule('apiq:OAR027');
+  return linter;
+});
+
+test('apiq:OAR027 should find errors', () => {
+  return linter.run(oar027fail).then((results) => {
+    expect(results.length).toBe(1);
+    expect(results[0].path.join('.')).toBe('paths./pets.post.responses.201');
+  });
+});
+
+test('apiq:OAR027 should find no errors', () => {
+  return linter.run(oar027ok).then((results) => {
+    expect(results.length).toBe(0);
+  });
+});
