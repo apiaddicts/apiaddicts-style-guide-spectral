ffd71b5ea3cb2f20a762a876a9beb3317ea297b2
Omar del Valle <omar@odelvalle.com>
2024-12-11 15:19:26 +0100

    rtefactoring 082 rule

--- test/oap3/format/oar037-string-schema.test.js
+++ test/oap3/format/oar037-string-schema.test.js
@@ -18,19 +18,19 @@ test('apiq:OAR037 should find errors', () => {
     expect(results[1].path.join(".")).toBe('paths./invoices.parameters.0.schema.format');
     expect(results[1].message).toBe('OAR037: Schema with type boolean should not specify format');
     expect(results[2].path.join(".")).toBe('paths./invoices.get.responses.200.content.application/json.schema.properties.binary.format');
-    expect(results[2].message).toBe('OAR037: Schema with type string has unrecognized format: binary');
+    expect(results[2].message).toBe('OAR037: Schema with type string has unrecognized format: Binari');
     expect(results[3].path.join(".")).toBe('paths./invoices.post.requestBody.content.application/json.schema.properties.byte.format');
-    expect(results[3].message).toBe('OAR037: Schema with type string has unrecognized format: byte');
+    expect(results[3].message).toBe('OAR037: Schema with type string has unrecognized format: Bite');
     expect(results[4].path.join(".")).toBe('paths./invoices.post.responses.200.content.application/json.schema.properties.byte.format');
-    expect(results[4].message).toBe('OAR037: Schema with type string has unrecognized format: byte');
+    expect(results[4].message).toBe('OAR037: Schema with type string has unrecognized format: Bite');
     expect(results[5].path.join(".")).toBe('paths./invoices.post.responses.200.content.application/json.schema.properties.binary.format');
-    expect(results[5].message).toBe('OAR037: Schema with type string has unrecognized format: binary');
+    expect(results[5].message).toBe('OAR037: Schema with type string has unrecognized format: Binari');
     expect(results[6].path.join(".")).toBe('paths./invoices.post.responses.200.content.application/json.schema.properties.other.items.format');
     expect(results[6].message).toBe('OAR037: Schema with type string has unrecognized format: other');
     expect(results[7].path.join(".")).toBe('paths./invoices/{invoiceId}.get.responses.200.content.application/json.schema.properties.byte.format');
-    expect(results[7].message).toBe('OAR037: Schema with type string has unrecognized format: byte');
+    expect(results[7].message).toBe('OAR037: Schema with type string has unrecognized format: Bite');
     expect(results[8].path.join(".")).toBe('paths./invoices/{invoiceId}.get.responses.200.content.application/json.schema.properties.binary.format');
-    expect(results[8].message).toBe('OAR037: Schema with type string has unrecognized format: binary');
+    expect(results[8].message).toBe('OAR037: Schema with type string has unrecognized format: Binari');
   });
 });
 
